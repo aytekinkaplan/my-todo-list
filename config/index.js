655f8d5756ed40a8a1f766eab147537b95038656
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-31 10:45:35 +0300

    Up

---
+++ config/index.js
@@ -0,0 +1,5 @@
+// config/index.js
+export { default as connectDB } from "./database.js";
+export { default as emailTransporter } from "./email.js";
+export { default as sessionConfig } from "./session.js";
+export { default as passportConfig } from "./passport.js";
